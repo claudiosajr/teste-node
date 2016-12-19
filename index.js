e6889dde6b7a80585ce4b2749d95bc20adbaadbe
Claudio Junior <claudio.sa.jr@gmail.com>
2016-12-19 02:32:07 -0200

    teste de porta

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 var express = require('express')
-var http = require('http');
-
 var app = express();
 
 app.set('view engine','ejs');
-
-app.get('/',function(req,res){
-    res.render("produtos/lista");
-});
 
 app.get('/produtos',function(req,res){
     res.render("produtos/lista");
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(process.env.PORT || 5000, function(){
+    console.log("servidor rodando");
 });
