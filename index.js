5ca75595f670ba8713feb5e55d54fcda89fe6374
Claudio Junior <claudio.sa.jr@gmail.com>
2016-12-19 02:12:06 -0200

    mais teste

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require('express')
+var http = require('http');
+
 var app = express();
 
 app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+    res.render("produtos/lista");
+});
 
 app.get('/produtos',function(req,res){
     res.render("produtos/lista");
 });
 
-app.listen(3000, function(){
-    console.log("servidor rodando");
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
